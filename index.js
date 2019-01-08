8bc1fedc173dde4d7464d16e3ad7e6afbd22933b
DESKTOP-QT8F8OK\hernan <hernag_09@hotmail.com>
2019-01-08 16:38:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+let app = express()
+
+app.set('port', process.env.PORT || 4000)
+
+app.listen(app.get('port'), (err) => {
+    if (err) console.log(`${err}`)
+
+    console.log('http://localhost:4000/new')
+})
+
+
+app.get('/new', (req, res) => {
+    res.status(200).send('hola mundo')
+
+
+})
