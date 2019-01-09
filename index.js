13db830a98d6026a0dddbc7acb2d1089131f8e1b
DESKTOP-QT8F8OK\hernan <hernag_09@hotmail.com>
2019-01-09 20:38:02 -0300

    first commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), (err) => {
     if (err) console.log(`${err}`)
 
-    console.log('http://localhost:4000')
+    console.log('http://localhost:4000/new')
 })
 
 
